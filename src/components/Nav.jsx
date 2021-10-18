7143a88c391580fdf7d80979ec82cbc739fcc171
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-18 15:52:04 +0100

    fetch and render Nav categories

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { getCategories } from "../utils/api"
+import { slugToName } from "../utils/string"
+
 const Nav = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((categoriesFromAPI) => {
+      setCategories(categoriesFromAPI)
+    })
+  }, [])
+
+  console.log(categories)
+
   return (
     <section>
       <ul>
-        <li>Link 1</li>
+        {categories.map((category) => (
+          <Link key={category.slug} to={`/categories/${category.slug}`}>
+            {slugToName(category.slug)}
+          </Link>
+        ))}
       </ul>
     </section>
   )
