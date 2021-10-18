46f75133f58c42347fab8f3b35154f240cbba6fb
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-18 16:29:34 +0100

    implement getReviews() helper

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,3 +8,8 @@ export const getCategories = async () => {
   const { data } = await gamesApi.get("/categories")
   return data.categories
 }
+
+export const getReviews = async () => {
+  const { data } = await gamesApi.get("/reviews")
+  return data.reviews
+}
