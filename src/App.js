ccc1ed0df604bdf25ba63dada09a9a7b7e4d5cf2
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-18 16:30:30 +0100

    render Reviews component on home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css"
 import Header from "./components/Header"
 import Nav from "./components/Nav"
+import Reviews from "./components/Reviews"
+import { Switch, Route } from "react-router"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Reviews />
+        </Route>
+      </Switch>
     </div>
   )
 }
