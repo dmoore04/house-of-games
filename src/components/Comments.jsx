27637e441c32c4e919a0aef51789f582431b47d6
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-20 15:39:33 +0100

    catch comment fetching errors

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -7,12 +7,17 @@ import { UserContext } from "../context/User"
 const Comments = ({ review_id }) => {
   const [comments, setComments] = useState([])
   const [newComment, setNewComment] = useState("")
+  const [error, setError] = useState(null)
   const { user } = useContext(UserContext)
 
   useEffect(() => {
-    getReviewComments(review_id).then((commentsFromAPI) => {
-      setComments(commentsFromAPI)
-    })
+    getReviewComments(review_id)
+      .then((commentsFromAPI) => {
+        setComments(commentsFromAPI)
+      })
+      .catch((err) => {
+        setError(err)
+      })
   }, [review_id])
 
   function addComment(e) {
@@ -28,24 +33,32 @@ const Comments = ({ review_id }) => {
   return (
     <section>
       <h3>Comments</h3>
-      <form action="" onSubmit={addComment}>
-        <input
-          type="text"
-          id="comment_body"
-          name="comment_body"
-          placeholder="Enter comment..."
-          maxLength="255"
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          required
-        />
-        <button>Post</button>
-      </form>
-      <ul className={styles.comment__list}>
-        {comments.map((comment) => (
-          <CommentCard key={comment.comment_id} comment={comment} />
-        ))}
-      </ul>
+      {error ? (
+        <p className={styles.error}>
+          Could not fetch comments, please refresh.
+        </p>
+      ) : (
+        <>
+          <form action="" onSubmit={addComment}>
+            <input
+              type="text"
+              id="comment_body"
+              name="comment_body"
+              placeholder="Enter comment..."
+              maxLength="255"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              required
+            />
+            <button>Post</button>
+          </form>
+          <ul className={styles.comment__list}>
+            {comments.map((comment) => (
+              <CommentCard key={comment.comment_id} comment={comment} />
+            ))}
+          </ul>
+        </>
+      )}
     </section>
   )
 }
