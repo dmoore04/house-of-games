beffe68248bc8a4427eadfb30d2283a83517618f
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-21 14:33:38 +0100

    style status code on generic 404 page

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -5,7 +5,9 @@ const Page404 = () => {
     <section>
       <div className={styles.container}>
         <p className={styles.construction}>🚧</p>
-        <h2 className={styles.title}>404: Not Found</h2>
+        <h2 className={styles.title}>
+          <span className={styles.status}>404:</span> Not Found
+        </h2>
         <p className={styles.description}>This page does not exist.</p>
       </div>
     </section>
