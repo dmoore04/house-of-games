d83473266168d85dd8a7cc8dd9b26f6314a1fc46
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-22 14:23:59 +0100

    refactor Votes components to work with new Voter

--- src/components/CommentVotes.jsx
+++ src/components/CommentVotes.jsx
@@ -1,24 +1,27 @@
 import { useState } from "react"
-import { CommentVoter } from "./Voter"
+import { Voter } from "./Voter"
+import { addCommentVote } from "../utils/api"
 
 const CommentVotes = ({ comment, setComments }) => {
   const [voted, setVoted] = useState(false)
+  // hacky solution because no GET comments/:comment_id endpoint
+  function addVote(value) {
+    setComments((currComments) =>
+      currComments.map((currComment) =>
+        currComment.comment_id === comment.comment_id
+          ? { ...currComment, votes: currComment.votes + value }
+          : currComment
+      )
+    )
+    addCommentVote(comment.comment_id, value)
+    setVoted(true)
+  }
+
   return (
     <span>
-      <CommentVoter
-        comment={comment}
-        setComments={setComments}
-        voted={voted}
-        setVoted={setVoted}
-        value={1}
-      />
-      <CommentVoter
-        comment={comment}
-        setComments={setComments}
-        voted={voted}
-        setVoted={setVoted}
-        value={-1}
-      />
+      {[1, -1].map((value) => (
+        <Voter key={value} addVote={addVote} value={value} voted={voted} />
+      ))}
       {comment.votes}
     </span>
   )
--- src/components/ReviewVotes.jsx
+++ src/components/ReviewVotes.jsx
@@ -1,9 +1,19 @@
 import styles from "../styles/ReviewVotes.module.css"
 import { Voter } from "./Voter"
 import { useState } from "react"
+import { addReviewVote } from "../utils/api"
 
 const ReviewVotes = ({ review, setReview }) => {
   const [voted, setVoted] = useState(false)
+
+  function addVote(value) {
+    setReview((currReview) => {
+      const newVotes = currReview.votes + value
+      return { ...currReview, votes: newVotes }
+    })
+    setVoted(true)
+    addReviewVote(review.review_id, value)
+  }
 
   return (
     <div className={styles.voting}>
@@ -13,14 +23,7 @@ const ReviewVotes = ({ review, setReview }) => {
       </p>
       <div className={styles.voters}>
         {[1, -1].map((value) => (
-          <Voter
-            key={value}
-            value={value}
-            review={review}
-            setReview={setReview}
-            voted={voted}
-            setVoted={setVoted}
-          />
+          <Voter key={value} value={value} voted={voted} addVote={addVote} />
         ))}
       </div>
     </div>
