628d59c7ef1a0efff218e4c335fcb04318a18c98
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-22 12:35:48 +0100

    indicate which voter was clicked

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -1,8 +1,12 @@
 import { addReviewVote } from "../utils/api"
 import styles from "../styles/Voter.module.css"
+import { useState } from "react"
 
 const Voter = ({ review, setReview, value, voted, setVoted }) => {
+  const [wasClicked, setWasClicked] = useState(false)
+
   function addVote() {
+    setWasClicked(true)
     setReview((currReview) => {
       const newVotes = currReview.votes + value
       return { ...currReview, votes: newVotes }
@@ -12,7 +16,13 @@ const Voter = ({ review, setReview, value, voted, setVoted }) => {
   }
 
   return (
-    <button disabled={voted} className={styles.voter} onClick={addVote}>
+    <button
+      disabled={voted}
+      className={`${styles.voter} ${
+        wasClicked && (value > 0 ? styles.upvote : styles.downvote)
+      }`}
+      onClick={addVote}
+    >
       {value > 0 ? "👍" : "👎"}
     </button>
   )
