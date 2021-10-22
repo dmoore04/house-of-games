228c9a72f6585620c9a3dbccbc8a87712eb24296
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-22 14:23:06 +0100

    make Voter component more generic

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -1,19 +1,8 @@
-import { addReviewVote, addCommentVote } from "../utils/api"
 import styles from "../styles/Voter.module.css"
 import { useState } from "react"
 
-export const Voter = ({ review, setReview, value, voted, setVoted }) => {
+export const Voter = ({ value, voted, addVote }) => {
   const [wasClicked, setWasClicked] = useState(false)
-
-  function addVote() {
-    setWasClicked(true)
-    setReview((currReview) => {
-      const newVotes = currReview.votes + value
-      return { ...currReview, votes: newVotes }
-    })
-    setVoted(true)
-    addReviewVote(review.review_id, value)
-  }
 
   return (
     <button
@@ -21,42 +10,10 @@ export const Voter = ({ review, setReview, value, voted, setVoted }) => {
       className={`${styles.voter} ${
         wasClicked && (value > 0 ? styles.upvote : styles.downvote)
       }`}
-      onClick={addVote}
-    >
-      {value > 0 ? "👍" : "👎"}
-    </button>
-  )
-}
-
-export const CommentVoter = ({
-  comment,
-  setComments,
-  voted,
-  setVoted,
-  value,
-}) => {
-  const [wasClicked, setWasClicked] = useState(false)
-
-  function addVote() {
-    setWasClicked(true)
-    setComments((currComments) =>
-      currComments.map((currComment) =>
-        currComment.comment_id === comment.comment_id
-          ? { ...currComment, votes: currComment.votes + value }
-          : currComment
-      )
-    )
-    addCommentVote(comment.comment_id, value)
-    setVoted(true)
-  }
-
-  return (
-    <button
-      className={`${
-        wasClicked && (value > 0 ? styles.upvote : styles.downvote)
-      }`}
-      disabled={voted}
-      onClick={addVote}
+      onClick={() => {
+        setWasClicked(true)
+        addVote(value)
+      }}
     >
       {value > 0 ? "👍" : "👎"}
     </button>
